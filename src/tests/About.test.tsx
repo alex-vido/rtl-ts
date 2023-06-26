7c9c2eff87c64f36a924db3fbc6490131067ec79
Alex Vido <alex.vido93@gmail.com>
2023-06-26 15:53:41 -0300

    Exercise 2: lint

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -7,16 +7,12 @@ test('Teste se o topo da aplicação contém um conjunto fixo de links de navega
   const btnHome = screen.getByRole('link', { name: /home/i });
   const btnAbout = screen.getByRole('link', { name: /about/i });
   const btnFavorite = screen.getByRole('link', { name: /favorite pokémon/i });
-  const title = screen.getByRole('heading', { name: /About Pokédex/i })
+  const title = screen.getByRole('heading', { name: /About Pokédex/i });
   const subTitle = screen.getByText(
-    /this application simulates a pokédex, a digital encyclopedia containing all pokémon/i
-    )
-  const paragraphy = screen.getByText(
-    /one can filter pokémon by type, and see more details for each one of them/i
-    )
-  const img = screen.getByRole('img', {
-    name: /pokédex/i
-  })
+  /this application simulates a pokédex, a digital encyclopedia containing all pokémon/i
+    );
+  const paragraphy = screen.getByText(/one can filter pokémon by type, and see more details for each one of them/i);
+  const img = screen.getByRole('img', { name: /pokédex/i });
   expect(btnHome).toBeInTheDocument();
   expect(btnAbout).toBeInTheDocument();
   expect(btnFavorite).toBeInTheDocument();
