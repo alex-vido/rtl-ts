fc2e962965f0e36ba0a69c9a768f2ffec5d542d8
Alex Vido <alex.vido93@gmail.com>
2023-06-26 16:22:11 -0300

    Exercise 3

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -8,8 +8,7 @@ test('Teste se o topo da aplicação contém um conjunto fixo de links de navega
   const btnAbout = screen.getByRole('link', { name: /about/i });
   const btnFavorite = screen.getByRole('link', { name: /favorite pokémon/i });
   const title = screen.getByRole('heading', { name: /About Pokédex/i });
-  const subTitle = screen
-    .getByText(/this application simulates a pokédex, a digital encyclopedia containing all pokémon/i);
+  const subTitle = screen.getByText(/this application simulates a pokédex, a digital encyclopedia containing all pokémon/i);
   const paragraphy = screen.getByText(/one can filter pokémon by type, and see more details for each one of them/i);
   const img = screen.getByRole('img', { name: /pokédex/i });
   expect(btnHome).toBeInTheDocument();
--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,28 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Teste para contemplar 100% dos casos de uso criados pelo Stryker', async () => {
+  const { user } = renderWithRouter(<App />, { route: '/favorites' });
+  const btnHhome = screen.getByRole('link', { name: /home/i });
+  const btnAbout = screen.getByRole('link', { name: /about/i });
+  const btnFavorite = screen.getByRole('link', { name: /favorite pokémon/i });
+  const title = screen.getByRole('heading', { name: /favorite pokémon/i });
+  const noPokemon = screen.getByText(/no favorite pokémon found/i);
+
+  expect(btnHhome).toBeInTheDocument();
+  expect(btnAbout).toBeInTheDocument();
+  expect(btnFavorite).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+  expect(noPokemon).toBeInTheDocument();
+  await user.click(btnHhome);
+  const btnDetails = screen.getByRole('link', { name: /more details/i });
+  expect(btnDetails).toBeInTheDocument();
+  await user.click(btnDetails);
+  const favorite = screen.getByText(/pokémon favoritado\?/i);
+  expect(favorite).toBeInTheDocument();
+  await user.click(favorite);
+  await user.click(btnFavorite);
+  const pikachu = screen.getByText(/pikachu/i);
+  expect(pikachu).toBeInTheDocument();
+});
