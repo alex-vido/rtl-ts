149b439557ce51ecaf0b4d260c89a58bca163aed
Alex Vido <alex.vido93@gmail.com>
2023-06-26 16:34:50 -0300

    Exercise 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Teste caso entre em um link inválido, entra na página not found', async () => {
+  renderWithRouter(<App />, { route: '/test' });
+  const btnHhome = screen.getByRole('link', { name: /home/i });
+  const btnAbout = screen.getByRole('link', { name: /about/i });
+  const btnFavorite = screen.getByRole('link', { name: /favorite pokémon/i });
+  const title = screen.getByRole('heading', { name: /page requested not found/i });
+  const img = screen
+    .getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i
+    }
+  );
+
+  expect(btnHhome).toBeInTheDocument();
+  expect(btnAbout).toBeInTheDocument();
+  expect(btnFavorite).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+  expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
